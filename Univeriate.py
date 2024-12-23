b8546d3458217b19e10587becebc49fb15ecd50d
pranava007 <pranavamuthu000@gmail.com>
2024-12-23 19:04:24 +0530

    completed

---
+++ Univeriate.py
@@ -0,0 +1,17 @@
+class Univeriate():
+    def quanQual(dataset):
+        
+        qual = []
+        quan = []
+        for columnName in dataset.columns:         
+            # print(columnName)
+            if(dataset[columnName].dtype == 'O'):
+                qual.append(columnName)
+            else:
+                quan.append(columnName)
+        return qual,quan
+       
+    
+
+
+    
